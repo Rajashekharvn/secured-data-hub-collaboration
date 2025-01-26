e5ed5e502f7eb148e240e7f782d8d52c2a6c4277
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-01-26 19:55:57 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,13 @@
 <?php
-// config/db.php
-$host = 'localhost';
-$dbname = 'secured_data_hub';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
